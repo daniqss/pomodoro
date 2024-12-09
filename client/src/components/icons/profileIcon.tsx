77fcdfc0042cafe27cf57e3a28b71e34df11438e
daniqss <danielqueijo14@gmail.com>
2024-12-09 12:16:51 +0100

    fix: concat className props in profile icon

--- client/src/components/icons/profileIcon.tsx
+++ client/src/components/icons/profileIcon.tsx
@@ -7,11 +7,11 @@ export default function ProfileIcon(props: React.SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={`icon icon-tabler icons-tabler-outline icon-tabler-user ${props.className}`}
       {...props}
-      //   class="icon icon-tabler icons-tabler-outline icon-tabler-user"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
