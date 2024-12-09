4218c46be498f49b0553a18e927ad929868b58fb
daniqss <danielqueijo14@gmail.com>
2024-12-09 23:57:06 +0100

    refactor: improve layout and styling in ConnectionData and UserList components

--- client/src/components/ConnectionData.tsx
+++ client/src/components/ConnectionData.tsx
@@ -18,28 +18,21 @@ function ConnectionData({ users }: { users: user[] }) {
 
   return (
     <section className="bg-zinc-800 shadow-lg rounded-lg overflow-hidden p-6 text-center">
-      <h3 className="text-white text-2xl font-bold mb-2">
-        Connected successfully!
-      </h3>
-      <p className="text-gray-400 mb-6">
+      <p className="text-gray-400 text-start mr-5">
         Share this room name with your partners to start working!
       </p>
-
-      <div className="bg-zinc-700 rounded-lg p-4 mb-6">
-        <h4 className="text-white text-lg mb-2">Room name:</h4>
-        <div className="flex items-center justify-center bg-zinc-600 rounded-md">
-          <p className="px-3 py-2 text-white">{room}</p>
-          <button
-            className="px-3 py-2 hover:bg-zinc-500 rounded-r-md transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50"
-            onClick={copyToClipboard}
-          >
-            <ClipboardIcon className="w-5 h-5 text-white" />
-          </button>
-        </div>
-        {copied && (
-          <p className="text-green-400 mt-2 text-sm">Copied to clipboard!</p>
-        )}
+      <div className="flex items-center justify-center bg-zinc-600 rounded-md">
+        <p className="px-3 py-2 text-white">{room}</p>
+        <button
+          className="px-3 py-2 hover:bg-zinc-500 rounded-r-md transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50"
+          onClick={copyToClipboard}
+        >
+          <ClipboardIcon className="w-5 h-5 text-white" />
+        </button>
       </div>
+      {copied && (
+        <p className="text-green-400 mt-2 text-sm">Copied to clipboard!</p>
+      )}
 
       <section>
         <h3 className="text-white text-xl font-semibold mb-3">
--- client/src/components/UserList.tsx
+++ client/src/components/UserList.tsx
@@ -7,11 +7,13 @@ export default function UserList({ users }: { users: user[] }) {
   const { userName, socket } = useContext(WsContext) as WsContextType;
 
   return (
-    <ul className="bg-zinc-700 rounded-md  space-y-2 pt-2 pb-1">
+    <ul className="bg-zinc-700 rounded-md  space-y-2 pt-2 pb-1 flex flex-col">
       {users.map((user, index) => (
         <li key={index} className="py-2 px-3">
           {index === 0 ? (
             <UserElement user={{ id: socket.id, name: userName }} />
+          ) : index === users.length - 1 ? (
+            <UserElement user={user} />
           ) : (
             <UserElement user={user} />
           )}
@@ -28,17 +30,19 @@ function UserElement({ user }: { user: user }) {
       <ProfileIcon className="w-6 h-6 text-gray-400 mr-1 mt-1" />
       <div className="flex flex-col sm:flex-row sm:items-center space-y-1 sm:space-y-0 sm:space-x-2 min-w-0 mt-1">
         {user.id === socket.id ? (
-          <h4 className="text-lg font-semibold truncate sm:text-base md:text-lg">
+          <h4 className="text-sm font-semibold truncate sm:text-base md:text-lg">
             {user.name}
           </h4>
         ) : (
-          <h4 className="text-lg truncate sm:text-base md:text-lg">
+          <h4 className="text-sm truncate sm:text-base md:text-lg">
             {user.name}
           </h4>
         )}
-        <p className="text-xs text-gray-400 truncate sm:text-[8px] md:text-xs pt-1 hidden sm:inline">
-          {user.id}
-        </p>
+        {user.id !== user.name && (
+          <p className="text-xs text-gray-400 truncate sm:text-[8px] md:text-xs pt-1 hidden md:inline">
+            {user.id}
+          </p>
+        )}
       </div>
     </article>
   );
