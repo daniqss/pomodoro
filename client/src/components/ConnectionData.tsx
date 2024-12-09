c25d305cf103b44a27eecca5a6ef312a22059568
daniqss <danielqueijo14@gmail.com>
2024-12-10 00:32:09 +0100

    fix: fix copy button not following container border

--- client/src/components/ConnectionData.tsx
+++ client/src/components/ConnectionData.tsx
@@ -18,24 +18,31 @@ function ConnectionData({ users }: { users: user[] }) {
 
   return (
     <section className="bg-zinc-800 shadow-lg rounded-lg overflow-hidden p-6 text-center">
-      <p className="text-gray-400 text-start mr-5">
-        Share this room name with your partners to start working!
-      </p>
-      <div className="flex items-center justify-center bg-zinc-600 rounded-md">
-        <p className="px-3 py-2 text-white">{room}</p>
+      <div className="flex flex-row items-center justify-between pb-2 space-x-2">
+        <p className="text-gray-400 text-sm sm:text-sm md:text-base text-start">
+          Share this room name with your partners to start working!
+        </p>
+        {copied && (
+          <p className="text-green-400 text-xs sm:text-sm shrink-0">
+            Copied to clipboard!
+          </p>
+        )}
+      </div>
+      <article className="flex items-stretch bg-zinc-600 rounded-md max-w-full overflow-hidden">
+        <p className="px-3 py-2 text-white text-xs sm:text-sm md:text-md lg:text-lg truncate flex-grow min-w-0">
+          {room}
+        </p>
         <button
-          className="px-3 py-2 hover:bg-zinc-500 rounded-r-md transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50"
+          className="px-3 py-2 bg-zinc-600 hover:bg-zinc-500 transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 flex items-center justify-center"
           onClick={copyToClipboard}
+          aria-label="Copy room name to clipboard"
         >
           <ClipboardIcon className="w-5 h-5 text-white" />
         </button>
-      </div>
-      {copied && (
-        <p className="text-green-400 mt-2 text-sm">Copied to clipboard!</p>
-      )}
+      </article>
 
       <section>
-        <h3 className="text-white text-xl font-semibold mb-3">
+        <h3 className="text-white text-xl text-start font-semibold mt-3 mb-2">
           Connected partners:
         </h3>
         <UserList users={users} />
